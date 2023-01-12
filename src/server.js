0c9f72e32799dbef9c5c8688d997fb2e70678665
Paulo Andreola <pauloandreola@gmail.com>
2023-01-12 13:03:34 -0300

    feat: Projet01 módulo02 parte02

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
+  const { method, url} = req;
+
+  if (method == 'GET' && url == '/users') {
+    return res.end(users);
+  }
+  if (method == 'POST' && url == '/users') {
+    users.push({
+      id: 1,
+      name: 'Paulo Andreola',
+      email: 'pauloandreola@gmail.com',
+    });
+    return res.end('User Create')
+  }
+
   return res.end('Hello World');
 });
 
