8a39481466c82837e3a854fca779062cfdee6fa3
Paulo Andreola <pauloandreola@gmail.com>
2023-01-12 13:32:01 -0300

    feat: http status code

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
 
   if (method == 'GET' && url == '/users') {
     return res
-    .setHeader('conttext-type', 'application/json')
+    .setHeader('context-type', 'application/json')
     .end(JSON.stringify(users));
   }
   if (method == 'POST' && url == '/users') {
@@ -16,10 +16,10 @@ const server = http.createServer((req, res) => {
       name: 'Paulo Andreola',
       email: 'pauloandreola@gmail.com',
     });
-    return res.end('User Create')
+    return res.writeHead(201).end();
   }
 
-  return res.end('Hello World');
+  return res.writeHead(404).end('Not found');
 });
 
 server.listen(3333);
